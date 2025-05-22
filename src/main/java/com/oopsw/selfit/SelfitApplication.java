2f084a3db0052df8f63140cbdc07c3489966b254
kokoko12334 <gnjgnjgnj1@gmail.com>
2025-05-22 18:20:53 +0900

    first commit

---
+++ src/main/java/com/oopsw/selfit/SelfitApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.selfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfitApplication.class, args);
+    }
+
+}
