5aed3a039fab91ebf705b6c3466b8034f307f11d
lrene4872@naver.com <lrene4872@naver.com>
2025-05-31 20:13:37 +0900

    test: ExerciseInfoRepositoryTests 주석 추가

--- src/test/java/com/oopsw/selfit/repository/ExerciseInfoRepositoryTests.java
+++ src/test/java/com/oopsw/selfit/repository/ExerciseInfoRepositoryTests.java
@@ -19,31 +19,25 @@ class ExerciseInfoRepositoryTests {
 	@Autowired
 	private ExerciseInfoRepository exerciseInfoRepository;
 
-	@Test
-	void testRemoveExercise() {
-		// given
-		ExerciseInfo exerciseInfo = ExerciseInfo.builder()
-			.exerciseId(8)
-			.exerciseMin(20)
-			.exerciseKcal(150)
-			.exerciseNoteId(200)
-			.build();
-		ExerciseInfo saved = exerciseInfoRepository.save(exerciseInfo);
-		Integer id = saved.getExerciseInfoId();
-
-		// when
-		exerciseInfoRepository.deleteById(id);
-
-		// then
-		assertFalse(exerciseInfoRepository.existsById(id));
-
-	}
-
-	@Test
-	void testGetExercisesForDate() {
-	}
-
-
+	//@Test
+	// void testRemoveExercise() {
+	// 	// given
+	// 	ExerciseInfo exerciseInfo = ExerciseInfo.builder()
+	// 		.exerciseId(8)
+	// 		.exerciseMin(20)
+	// 		.exerciseKcal(150)
+	// 		.exerciseNoteId(200)
+	// 		.build();
+	// 	ExerciseInfo saved = exerciseInfoRepository.save(exerciseInfo);
+	// 	Integer id = saved.getExerciseInfoId();
+	//
+	// 	// when
+	// 	exerciseInfoRepository.deleteById(id);
+	//
+	// 	// then
+	// 	assertFalse(exerciseInfoRepository.existsById(id));
+	//
+	// }
 }
 
 
