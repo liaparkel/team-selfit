b8274e8573bcd9e034f52bd8c0133518167627fe
KOMINPYO <gnjgnjgnj1@gmail.com>
2025-06-01 17:55:11 +0900

    feat: 유저 회원가입 Service에도 검증 추가

--- src/main/java/com/oopsw/selfit/auth/CustomOAuth2FailureHandler.java
+++ src/main/java/com/oopsw/selfit/auth/CustomOAuth2FailureHandler.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -14,10 +13,8 @@ import jakarta.servlet.http.HttpSession;
 @Component
 public class CustomOAuth2FailureHandler implements AuthenticationFailureHandler {
 	@Override
-	public void onAuthenticationFailure(HttpServletRequest request,
-		HttpServletResponse response,
-		AuthenticationException exception)
-		throws IOException, ServletException {
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException exception) throws IOException {
 
 		HttpSession session = request.getSession(false);
 		String email = (String)session.getAttribute("email");
