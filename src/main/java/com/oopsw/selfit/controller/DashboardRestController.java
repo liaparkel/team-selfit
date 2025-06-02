1a0c6b0eeb49832cc19ecf600a94a53683deb2ae
YunhoYeom <whatever.u.r1103@gmail.com>
2025-06-02 09:28:25 +0900

    refactor: DashboardRestController로 JPA refactoring 완료

--- src/main/java/com/oopsw/selfit/controller/DashboardRestController.java
+++ src/main/java/com/oopsw/selfit/controller/DashboardRestController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oopsw.selfit.dto.Checklist;
+import com.oopsw.selfit.dto.Exercise;
 import com.oopsw.selfit.dto.Food;
 import com.oopsw.selfit.service.CheckService;
 import com.oopsw.selfit.service.DashboardService;
+import com.oopsw.selfit.service.ExerciseInfoService;
 import com.oopsw.selfit.service.FoodInfoService;
 import com.oopsw.selfit.service.MemberService;
 
@@ -28,6 +30,7 @@ public class DashboardRestController {
 	private final MemberService memberService;
 	private final FoodInfoService foodInfoService;
 	private final CheckService checkService;
+	private final ExerciseInfoService exerciseInfoService;
 
 	@PostMapping("/checklist/items")
 	public List<Checklist> getCheckList(@RequestBody Checklist checklist) {
@@ -84,7 +87,44 @@ public class DashboardRestController {
 
 	@PostMapping("/foods")
 	public List<Food> getFoodInfos(@RequestBody Map<String, Object> foods) {
-		Food food=Food.builder().intakeDate((String)foods.get("intakeDate")).memberId((int)foods.get("memberId")).build();
+		Food food=Food.builder()
+			.intakeDate((String)foods.get("intakeDate"))
+			.memberId((int)foods.get("memberId"))
+			.build();
 		return foodInfoService.getFoodInfoList(food);
 	}
+
+	@DeleteMapping("/exercise")
+	public ResponseEntity<String> removeExerciseInfo(@RequestBody Map<String, Integer> exerciseInfoId) {
+		exerciseInfoService.removeExercise(exerciseInfoId.get("exerciseInfoId"));
+		return ResponseEntity.ok().body("OK");
+	}
+
+	@PutMapping("/exercise")
+	public ResponseEntity<String> setExerciseMin(@RequestBody Map<String, Integer> exerciseInfo) {
+		exerciseInfoService.setExerciseMin(exerciseInfo.get("exerciseInfoId"), exerciseInfo.get("newMin"));
+		return ResponseEntity.ok().body("OK");
+	}
+
+	@PostMapping("/exercise")
+	public ResponseEntity<String> addExerciseInfo(@RequestBody Map<String, Object> exerciseInfo) {
+		Exercise e=Exercise.builder()
+			.exerciseNoteId((int)exerciseInfo.get("exerciseNoteId"))
+			.exerciseMin((int)exerciseInfo.get("exerciseMin"))
+			.exerciseName((String)exerciseInfo.get("exerciseName"))
+			.met(((Number) exerciseInfo.get("met")).floatValue())
+			.build();
+		exerciseInfoService.addExerciseInfo(e);
+		return ResponseEntity.ok().body("OK");
+	}
+
+	@PostMapping("/exercises")
+	public List<Exercise> getExerciseInfos(@RequestBody Map<String, Object> exercises) {
+		Exercise exercise = Exercise.builder()
+			.exerciseDate((String)exercises.get("exerciseDate"))
+			.memberId((int)exercises.get("memberId"))
+			.build();
+		return exerciseInfoService.getExerciseInfoList(exercise);
+	}
+
 }
